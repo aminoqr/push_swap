52b49c645e2861bf11f8424a0eac4d96c46e41b8
aminoqr <aminoqr12@gmail.com>
2025-12-24 19:35:57 +0100

    feat: implement push operations - pa, pb

--- push.c
+++ push.c
@@ -6,35 +6,36 @@
 /*   By: aasylbye <aasylbye@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/14 18:39:11 by aasylbye          #+#    #+#             */
-/*   Updated: 2025/12/14 18:39:43 by aasylbye         ###   ########.fr       */
+/*   Updated: 2025/12/24 18:49:35 by aasylbye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int ft_push(t_stack **src, t_stack **dest)
+int	ft_push(t_stack **src, t_stack **dest)
 {
-	t_stack *temp;
+	t_stack	*temp;
+
 	if (!*src)
-		return 0;
+		return (0);
 	temp = *src;
 	ft_remove_front(src);
 	ft_add_front(dest, temp);
-	return 1;
+	return (1);
 }
 
-int pa(t_stack **stack_a, t_stack **stack_b)
+int	pa(t_stack **stack_a, t_stack **stack_b)
 {
-	if(!ft_push(stack_b, stack_a))
-		return 0;
-	printf("pa\n");
-	return 1;
+	if (!ft_push(stack_b, stack_a))
+		return (0);
+	write(1, "pa\n", 3);
+	return (1);
 }
 
-int pb(t_stack **stack_a, t_stack **stack_b)
+int	pb(t_stack **stack_a, t_stack **stack_b)
 {
-	if(!ft_push(stack_a, stack_b))
-		return 0;
-	printf("pb\n");
-	return 1;
+	if (!ft_push(stack_a, stack_b))
+		return (0);
+	write(1, "pb\n", 3);
+	return (1);
 }
