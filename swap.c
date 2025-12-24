f58e08b24709aac10712fdf07cb1c9fee3ad3071
aminoqr <aminoqr12@gmail.com>
2025-12-24 19:35:35 +0100

    feat: implement swap operations - sa, sb, ss

--- swap.c
+++ swap.c
@@ -6,7 +6,7 @@
 /*   By: aasylbye <aasylbye@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/14 18:38:24 by aasylbye          #+#    #+#             */
-/*   Updated: 2025/12/14 18:39:03 by aasylbye         ###   ########.fr       */
+/*   Updated: 2025/12/24 18:44:45 by aasylbye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,45 +14,46 @@
 
 int	ft_swap(t_stack **stack)
 {
-	t_stack *temp;
+	t_stack	*temp;
 
-	if(!*stack || !(*stack) -> next)
-		return 0;
+	if (!*stack || !(*stack)->next)
+		return (0);
 	temp = *stack;
-	*stack = (*stack) -> next;
-	(*stack) -> prev = NULL;
-	temp -> next = (*stack) -> next;
-	(*stack) -> next = temp;
-	temp -> prev = *stack;
+	*stack = (*stack)->next;
+	(*stack)->prev = NULL;
+	temp->next = (*stack)->next;
+	(*stack)->next = temp;
+	temp->prev = *stack;
 	if (temp->next)
 		temp->next->prev = temp;
-	return 1;
+	return (1);
 }
 
-int sa(t_stack **stack_a)
+int	sa(t_stack **stack_a)
 {
-	if(!ft_swap(stack_a))
-		return 0;
-	printf("sa\n");
-	return 1;
+	if (!ft_swap(stack_a))
+		return (0);
+	write(1, "sa\n", 3);
+	return (1);
 }
 
-int sb(t_stack **stack_b)
+int	sb(t_stack **stack_b)
 {
-	if(!ft_swap(stack_b))
-		return 0;
-	printf("sb\n");
-	return 1;
+	if (!ft_swap(stack_b))
+		return (0);
+	write(1, "sb\n", 3);
+	return (1);
 }
-int ss(t_stack **stack_a, t_stack **stack_b)
+
+int	ss(t_stack **stack_a, t_stack **stack_b)
 {
-	int result_a;
-	int result_b;
+	int	result_a;
+	int	result_b;
 
 	result_a = ft_swap(stack_a);
 	result_b = ft_swap(stack_b);
-	if(!result_a && !result_b)
-		return 0;
-	printf("ss\n");
-	return 1;
+	if (!result_a && !result_b)
+		return (0);
+	write(1, "ss\n", 3);
+	return (1);
 }
