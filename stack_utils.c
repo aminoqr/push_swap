749b3469165544a71d1f6501601b1b8cd7fe9eb3
aminoqr <aminoqr12@gmail.com>
2025-12-24 19:35:13 +0100

    feat: add stack operations - new node, add and remove functions

--- stack_utils.c
+++ stack_utils.c
@@ -6,35 +6,33 @@
 /*   By: aasylbye <aasylbye@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/14 13:40:44 by aasylbye          #+#    #+#             */
-/*   Updated: 2025/12/23 16:44:08 by aasylbye         ###   ########.fr       */
+/*   Updated: 2025/12/24 18:53:24 by aasylbye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-t_stack *ft_new_node(int value)
+t_stack	*ft_new_node(int value)
 {
-	t_stack *new_node;
-	
+	t_stack	*new_node;
+
 	new_node = malloc(sizeof(t_stack));
 	if (!new_node)
-		return NULL;
-	
-	new_node -> value = value;
-	new_node -> prev = NULL;
-	new_node -> next = NULL;
-	new_node -> target_node = NULL;
-	new_node -> index = -1;
-	new_node -> push_cost = 0;
-	new_node -> above_median = -1;
-	new_node -> cheapest = 0;
-	
-	return new_node;
+		return (NULL);
+	new_node->value = value;
+	new_node->prev = NULL;
+	new_node->next = NULL;
+	new_node->target_node = NULL;
+	new_node->index = -1;
+	new_node->push_cost = 0;
+	new_node->above_median = -1;
+	new_node->cheapest = 0;
+	return (new_node);
 }
 
-void ft_add_back(t_stack **stack, t_stack *new_node)
+void	ft_add_back(t_stack **stack, t_stack *new_node)
 {
-	t_stack *temp;
+	t_stack	*temp;
 
 	if (!new_node || !stack)
 		return ;
@@ -44,14 +42,14 @@ void ft_add_back(t_stack **stack, t_stack *new_node)
 		return ;
 	}
 	temp = *stack;
-	while(temp -> next)
-		temp = temp -> next;
-	temp -> next = new_node;
-	new_node -> prev = temp;
-	new_node -> next = NULL;
+	while (temp->next)
+		temp = temp->next;
+	temp->next = new_node;
+	new_node->prev = temp;
+	new_node->next = NULL;
 }
 
-void ft_add_front(t_stack **stack, t_stack *new_node)
+void	ft_add_front(t_stack **stack, t_stack *new_node)
 {
 	if (!stack || !new_node)
 		return ;
@@ -59,116 +57,34 @@ void ft_add_front(t_stack **stack, t_stack *new_node)
 		(*stack)->prev = new_node;
 	new_node->next = *stack;
 	*stack = new_node;
-	new_node -> prev = NULL;
+	new_node->prev = NULL;
 }
-void ft_remove_front(t_stack **stack)
+
+void	ft_remove_front(t_stack **stack)
 {
+	t_stack	*temp;
+
 	if (!stack || !*stack)
 		return ;
-		
-	t_stack *temp;
 	temp = *stack;
 	*stack = temp->next;
 	if (*stack)
-		(*stack) -> prev = NULL;
+		(*stack)->prev = NULL;
 }
 
-void ft_remove_back(t_stack **stack)
+void	ft_remove_back(t_stack **stack)
 {
-	t_stack *temp;
+	t_stack	*temp;
+
 	if (!stack)
 		return ;
 	temp = *stack;
-	if(!temp->next)
+	if (!temp->next)
 	{
 		*stack = NULL;
-		return ;	
+		return ;
 	}
-	while(temp->next->next)
-		temp = temp -> next;
-	temp -> next = NULL;
-}
-
-int ft_is_duplicate(t_stack *stack, int value)
-{
-	if (!stack)
-		return 0;
-	while(stack)
-	{
-		if (stack -> value == value)
-			return 1;
-		stack = stack -> next;
-	}
-	return 0;
-}
-
-int ft_is_sorted(t_stack *stack)
-{
-	if (!stack)
-		return (1);
-	while(stack->next)
-	{
-		if (stack->value > stack->next->value)
-			return 0;
-		stack = stack->next;
-	}
-	return 1;
-}
-
-t_stack *ft_find_max(t_stack **stack)
-{
-	t_stack *temp;
-	t_stack *max;
-	if (!stack || !*stack)
-		return NULL;
-	temp = *stack;
-	max = *stack;
-	while(temp)
-	{
-		if (temp->value > max->value)
-			max = temp;
+	while (temp->next->next)
 		temp = temp->next;
-	}
-	return max;
-}
-
-t_stack *ft_find_min(t_stack **stack)
-{
-	t_stack *temp;
-	t_stack *min;
-	if (!stack || !*stack)
-		return NULL;
-	temp = *stack;
-	min = *stack;
-	while(temp)
-	{
-		if (temp->value < min-> value)
-			min = temp;
-		if (!temp->next)
-			break;
-		temp = temp->next;
-	}
-	return min;
-}
-
-void set_current_position(t_stack *stack)
-{
-	int index;
-	int size;
-	
-	if (!stack)
-		return;
-	index = 0;
-	size = stack_size(&stack);
-	while(stack)
-	{
-		stack->index = index;
-		
-		if (index <= size/2)
-			stack->above_median = 1;
-		else
-			stack -> above_median = 0; 
-		index++;
-		stack = stack->next;
-	}
+	temp->next = NULL;
 }
